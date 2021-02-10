e47bcdbb514ee61a26d0f90288ad1c78e4ffcfa8
wsierra010 <wsierra010@gmail.com>
2021-02-10 17:40:03 +0100

    Dashboard Finish

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,8 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<?php
+
+require_once($_SERVER["DOCUMENT_ROOT"] . '/PHP-EMPLOYEE-MANAGEMENT-V1/src/library/sessionHelper.php');
+require("../assets/html/header.html");
+
+
+?>
--- src/employee.php
+++ src/employee.php
@@ -1,1 +1,7 @@
 <!-- TODO Employee view -->
+<?php
+
+require_once($_SERVER["DOCUMENT_ROOT"] . '/PHP-EMPLOYEE-MANAGEMENT-V1/src/library/sessionHelper.php');
+require("../assets/html/header.html");
+
+?>
