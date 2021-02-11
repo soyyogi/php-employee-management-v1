39d7e589f2f621893ab3dd651ad39797f5e16296
wsierra010 <wsierra010@gmail.com>
2021-02-11 16:23:16 +0100

    Dashboard HTML

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,6 +1,19 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
-<?php
-require_once("./assets/html/header.html");
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dashboard</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php require_once("./assets/html/header.html");?>
+    <div id="jsGrid"></div>
 
-?>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
+    <script src="../assets/js/index.js"></script>
+</body>
+</html>
