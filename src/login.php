04df923eddb34fc2d7eea9d0f64853bce6d42135
wsierra010 <wsierra010@gmail.com>
2021-02-11 16:29:18 +0100

    LoginCSS

--- src/login.php
+++ src/login.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/css/login.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
     <title>EMS</title>
 </head>
 
